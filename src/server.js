dc98bbfbba3c1f13d6a3fa5ccb33e671d3d7e787
Onuwa Nnachi Isaac <matrix4u2002@gmail.com>
2019-01-31 00:37:29 +0100

    Add node-limiter

--- src/server.js
+++ src/server.js
@@ -4,8 +4,10 @@ const debug = require('debug')('api');
 const helmet = require('helmet');
 const http = require('http');
 const proxy = require('express-http-proxy');
-const redis = require('redis');
 const Prometheus = require('./prometheus');
+const RateLimter = require('limiter').RateLimiter;
+
+const limiter = new RateLimter(150, 'second');
 
 app.use(helmet({
   hsts: false,
@@ -15,7 +17,10 @@ app.use(helmet({
 app.use(Prometheus.requestCounters);
 app.use(Prometheus.responseCounters);
 Prometheus.injectMetricsRoute(app);
-Prometheus.startCollection();
+
+limiter.removeTokens(1, (err, remainingRequests) => {
+  Prometheus.startCollection();
+})
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -28,21 +33,6 @@ app.use((req, res, next) => {
   }
 
   next();
-});
-
-const redisServer = process.env.REDIS_URL || '';
-const client = redis.createClient(redisServer);
-const limiter = require('express-limiter')(app, client);
-
-const expireInSeconds = process.env.EXPIRE_IN_SECONDS || 15;
-const requestLimit = process.env.REQUEST_LIMIT || 99;
-const limitByIP = process.env.LIMIT_BY_IP ? 'connection.remoteAddress' : 'hostname';
-limiter({
-  path: '*',
-  method: 'all',
-  lookup: limitByIP,
-  total: requestLimit,
-  expire: 1000 * expireInSeconds,
 });
 
 const target = process.env.BACK_END_URL || 'http://localhost:3000';
